0ddee008fde18532b621de9a1addb13a2ff5316a
Colby M. White <colbywhite@users.noreply.github.com>
2023-06-24 16:30:26 -0500

    lint `yarn format`

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,14 +1,17 @@
 export default function About() {
   return (
     <main className="min-h-screen">
-      <section className="flex flex-col gap-2 justify-center">
+      <section className="flex flex-col justify-center gap-2">
         <h2 className="mx-auto mt-4 text-center text-xl sm:text-2xl">
           This is the <span className="italic">About</span> page.
         </h2>
-        <img width="480" height="320"
-             className="border border-black mx-auto"
-             src="https://source.unsplash.com/qdP8EYxdzL4/480x320"
-             alt="A golf club about to strike a golf ball"/>
+        <img
+          width="480"
+          height="320"
+          className="mx-auto border border-black"
+          src="https://source.unsplash.com/qdP8EYxdzL4/480x320"
+          alt="A golf club about to strike a golf ball"
+        />
       </section>
     </main>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,17 @@
 export default function Index() {
   return (
     <main className="min-h-screen">
-      <section className="flex flex-col gap-2 justify-center">
+      <section className="flex flex-col justify-center gap-2">
         <h2 className="mx-auto mt-4 text-center text-xl sm:text-2xl">
           This is the <span className="italic">Home</span> page.
         </h2>
-        <img width="480" height="320"
-             className="border border-black mx-auto"
-             src="https://source.unsplash.com/N21z4eG8aKg/480x320"
-             alt="A golf ball on the adege of a golf hole"/>
+        <img
+          width="480"
+          height="320"
+          className="mx-auto border border-black"
+          src="https://source.unsplash.com/N21z4eG8aKg/480x320"
+          alt="A golf ball on the adege of a golf hole"
+        />
       </section>
     </main>
   );
